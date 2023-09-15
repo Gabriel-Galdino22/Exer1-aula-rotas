61ebbbded9ae98f3569e3a907458809bb5915f1d
Gabriel-Galdino22 <rm550711@fiap.com.br>
2023-09-15 15:03:09 -0300

    Corrigindo erro de sintax.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'
 import Home from './routes/Home/index.jsx'
 import Produtos from './routes/Produtos/index.jsx'
-import Error from './routes/Error/Index.jsx'
+import Error from './routes/Error/index.jsx'
 import EditarProduto from './routes/EditarProduto/index.jsx'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
