50f7d54316bb9e9197232b7007c9c4675d826b35
Gabriel-Galdino22 <rm550711@fiap.com.br>
2023-09-15 17:21:23 -0300

    Corrigindo import.

--- src/routes/Produtos/index.jsx
+++ src/routes/Produtos/index.jsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom';
 import { listaProdutos } from '../../listaProdutos';
-
 export default function Produtos() {
     return (
         <main>
@@ -19,4 +18,4 @@ export default function Produtos() {
             </div>
         </main>
     );
-}
+}s
