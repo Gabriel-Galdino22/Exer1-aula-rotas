27e134ae70f0814586ef1f110355144d543dd15e
Gabriel-Galdino22 <rm550711@fiap.com.br>
2023-09-15 14:48:53 -0300

    Criando a lista de aparelhos na pasta .js

---
+++ src/ListaProduto.js
@@ -0,0 +1,39 @@
+export const listaProdutos = [
+    {
+        id:1,
+        nome:'Fone de Ouvido - sem fio',
+        descricao: 'O melhor para reuniões',
+        preco:'R$580,80' ,
+        imagem: "img\fonesSemFio.jpg"
+        
+    },
+    { 
+        id: 2, 
+        nome:'Televisão LG',
+        descricao: 'A Melhor imagem para sua noite de filme',
+        preco:'R$4.975,00',
+        imagem: "img\TvLG.png"
+    },
+    {
+        id:3,
+        nome:'Xiaomi Redmi Note 8',
+        descricao: 'Ótimo desempenho e duração de bateria',
+        preco:'R$1.875,99' ,
+        imagem: "img\RedmiNote8.png"
+        
+    },
+    {
+        id:4,
+        nome:'Iphone 14',
+        descricao: 'Considerado com melhor resulução na camera traseira',
+        preco: 'R$8.999,99',
+        imagem: "img\Iphone14.png"
+    },
+    {
+        id:5,
+        nome:'Teclado',
+        descricao: 'Teclas mecânicas e removiveis para melhor limpeza',
+        preco: 'R$926,00',
+        imagem: "img\Teclado.png"
+    },
+]
