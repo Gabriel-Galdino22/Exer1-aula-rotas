da07ec37ef87cc8cccc8d783c3df9922c9a4638c
Gabriel-Galdino22 <rm550711@fiap.com.br>
2023-09-15 15:32:01 -0300

    Adicionando rotas para os produtos.

--- src/routes/Produtos/index.jsx
+++ src/routes/Produtos/index.jsx
@@ -1,8 +1,23 @@
-export default function Produtos(){
+import { Link } from 'react-router-dom';
+import { listaProdutos } from '../../listaProdutos';
+ 
 
-    return(
+export default function Produtos() {
+    return (
         <main>
             <h1>Produtos</h1>
+            <div className="produto-container">
+                {listaProdutos.map(prod => (
+                    <div key={prod.id} className="produto">
+                        <h2>{prod.nome}</h2>
+                        <img src={prod.imagem} alt={prod.nome} />
+                        
+                        <p>Preço: R${prod.preco}</p>
+                        <Link to={`/produto/editar/${prod.id}`}>Editar</Link>
+                        <Link to={`/produto/visualizar/${prod.id}`}>Visualizar</Link>
+                    </div>
+                ))}
+            </div>
         </main>
-)
+    );
 }
